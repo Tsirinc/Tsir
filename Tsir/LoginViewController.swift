3ed1ad4d2617b38a792516455b7b4e82ef72364c
flaaghara <bullocktrevor123@yahoo.com>
2017-09-18 04:39:04 -0400

    Built login screen interface and added basic functionality to text boxes. Created registration file reference and added registration view to Interface Builder

---
+++ Tsir/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  Tsir
+//
+//  Created by Trevor Bullock on 9/16/17.
+//  Copyright © 2017 TSIR Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Manage username and password text field input through delegate callbacks
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+}
+
